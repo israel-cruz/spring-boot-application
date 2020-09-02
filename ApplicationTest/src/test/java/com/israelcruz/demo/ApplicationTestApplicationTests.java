207663283961e4bb45b27bf26147db92d0e23ae4
Israel Cruz <israel.crz@outlook.com>
2020-09-01 19:10:30 -0500

    Commit inicial con eclipse

---
+++ ApplicationTest/src/test/java/com/israelcruz/demo/ApplicationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israelcruz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
